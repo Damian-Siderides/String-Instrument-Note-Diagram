02e6c2789ef0d656e80fe0a7fdcd645eb9b38d9e
Damian-Siderides <siderides@live.com.au>
2025-01-17 11:56:28 +1100

    Improved Display, Added Extra Octave

--- note-diagram.py
+++ note-diagram.py
@@ -1,14 +1,27 @@
 notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 def print_string(note):
-	test = notes.index(note)
+	start = notes.index(note) # this is the section to fix, perhaps .find() will assist
 
-	print('[', end='')
-	for i in range(len(notes) + 1):
-		print(notes[(i + test) % (len(notes))], end='')
-		if i != len(notes):
-			print('-', end='')
-	print(']')
+	octaves = 2
+	size = len(notes)
+
+	# print('[', end='')
+	for i in range(size * octaves + 1):
+		if (i % size) == 0:
+			print('[', end='')
+
+		print(notes[(start + i) % size], end='')
+		
+		if (i % size) == 0:
+			print(']', end='')
+		
+		if i >= 0 and i < (size * octaves):
+			if len(notes[(start + i) % size]) == 1:
+				print('---', end='')
+			else:
+				print('--', end='')
+	print()
 
 def print_instrument(instrument):
 	print(f'{str(instrument['key'])} Note Chart\n')
